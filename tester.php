3095fe66f155c02bda7b8dda851571232733cc23
PreFiXAUT <me@prefixaut.net>
2017-08-13 21:07:00 +0200

    Removed tester
    Useless since unit-tests work (at least locally. fixing travis rn)

--- tester.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-require __DIR__ . '/vendor/autoload.php';
-
-$api = new Kamui\API(trim(@file_get_contents('./client_id.txt')));
-
-var_dump($api->channels->followers('439876893475689346789347689347689346gg'));
